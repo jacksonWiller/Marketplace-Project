553a7d0169f9ca072e64f714d7cb61c5d0277af4
Jackson Duarte <jacksonwillerduarte@gmail.com>
2021-09-03 23:02:26 -0300

    recebendo formData angular( em andamento)

--- AplicationApp/Dtos/ProdutoDto.cs
+++ AplicationApp/Dtos/ProdutoDto.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.IO;
 using Domain.Entity;
+using Microsoft.AspNetCore.Http;
 
 namespace AplicationApp.Dtos
 {
@@ -17,12 +19,12 @@ namespace AplicationApp.Dtos
         // [Required(ErrorMessage = "O campo {0} é obrigatório")]   
         // [StringLength(50, MinimumLength = 3,
         // ErrorMessage = "Intervalo permitido de 3 a 50 caracteres.")]
-        public string Descricao { get; set; }
+        // public string Descricao { get; set; }
 
         // [Required(ErrorMessage = "O campo {0} é obrigatório")]   
         // [StringLength(50, MinimumLength = 3,
         // ErrorMessage = "Intervalo permitido de 3 a 50 caracteres.")]
-        public string Observacao { get; set; }
+        // public string Observacao { get; set; }
 
         // [Required(ErrorMessage = "O campo {0} é obrigatório")]   
         // [Range(1, 120000, ErrorMessage = "{0} não pode ser menor que 1 e maior que 120.000")]
@@ -31,12 +33,13 @@ namespace AplicationApp.Dtos
         // [Required(ErrorMessage = "O campo {0} é obrigatório")]   
         // [Range(1, 200, ErrorMessage = "{0} não pode ser menor que 1 e maior que 120.000")]
 
-
+        public IFormFile Imagem { get; set; }
         public string ImagemURL { get; set; }
-        public int QuantidadeEmEstoque { get; set; }
-        public bool Estado { get; set; }
-        public DateTime DataCadastro { get; set; }
-        public DateTime DataAlteracao { get; set; }
-        public virtual ICollection<ProdutosCategoriasDto> ProdutosCategorias { get; set; }
+        // public int QuantidadeEmEstoque { get; set; }
+        // public bool Estado { get; set; }
+        // public DateTime DataCadastro { get; set; }
+        // public DateTime DataAlteracao { get; set; }
+        // public virtual ICollection<ProdutosCategoriasDto> ProdutosCategorias { get; set; }
+
     }
 }
