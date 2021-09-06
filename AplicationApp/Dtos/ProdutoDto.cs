afb88204d5f7654321111ad44055cbf18cca7afd
Jackson Duarte <jacksonwillerduarte@gmail.com>
2021-09-06 00:47:31 -0300

    corrigindo o identity

--- AplicationApp/Dtos/ProdutoDto.cs
+++ AplicationApp/Dtos/ProdutoDto.cs
@@ -19,12 +19,12 @@ namespace AplicationApp.Dtos
         // [Required(ErrorMessage = "O campo {0} é obrigatório")]   
         // [StringLength(50, MinimumLength = 3,
         // ErrorMessage = "Intervalo permitido de 3 a 50 caracteres.")]
-        // public string Descricao { get; set; }
+        public string Descricao { get; set; }
 
         // [Required(ErrorMessage = "O campo {0} é obrigatório")]   
         // [StringLength(50, MinimumLength = 3,
         // ErrorMessage = "Intervalo permitido de 3 a 50 caracteres.")]
-        // public string Observacao { get; set; }
+        public string Observacao { get; set; }
 
         // [Required(ErrorMessage = "O campo {0} é obrigatório")]   
         // [Range(1, 120000, ErrorMessage = "{0} não pode ser menor que 1 e maior que 120.000")]
@@ -35,11 +35,11 @@ namespace AplicationApp.Dtos
 
         public IFormFile Imagem { get; set; }
         public string ImagemURL { get; set; }
-        // public int QuantidadeEmEstoque { get; set; }
-        // public bool Estado { get; set; }
-        // public DateTime DataCadastro { get; set; }
-        // public DateTime DataAlteracao { get; set; }
-        // public virtual ICollection<ProdutosCategoriasDto> ProdutosCategorias { get; set; }
+        public int QuantidadeEmEstoque { get; set; }
+        public bool Estado { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public DateTime DataAlteracao { get; set; }
+        public virtual ICollection<ProdutosCategoriasDto> ProdutosCategorias { get; set; }
 
     }
 }
